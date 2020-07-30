664559073937af94c7483406dac9d3899d4072b9
Sreejith R <srramakr@microsoft.com>
2020-07-31 02:54:38 +0530

    Fix

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 pip install -r requirements-latest.txt
+apt-get install libglib2.0-0
 python core.py --config config/vox-256.yaml --checkpoint ~/Downloads/vox-cpk.pth.tar --relative --adapt_scale --source_image ~/Downloads/02.png --cpu
