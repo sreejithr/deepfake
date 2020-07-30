5a08fbd7a9d94e98f4e6df870490ed1b250c4b70
Sreejith R <srramakr@microsoft.com>
2020-07-31 02:40:11 +0530

    Fix

--- run.sh
+++ run.sh
@@ -1,2 +1,7 @@
 pip install -r requirements-latest.txt
+git clone https://github.com/1adrianb/face-alignment
+cd face-alignment
+pip install -r requirements.txt
+python setup.py install
+cd ..
 python core.py --config config/vox-256.yaml --checkpoint ~/Downloads/vox-cpk.pth.tar --relative --adapt_scale --source_image ~/Downloads/02.png --cpu
